6f109d187e9e73d0251a1515b2b1b078c1c4edf8
Father Cain <confess@fathercain.com>
2012-06-23 20:52:45 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirtApp::Application.config.secret_token = 'c53654ca3a92d495a46099b5de0a041dfebaf8bccd7d442af2eb5baf32fa4c0c41c503e270f956931e67706956334c6088722ea5e52a03e7fd2d2b49b77051be'
